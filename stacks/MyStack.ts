f9e412b154fd85c56d24643f3680b91479999f2b
Diogo Cardoso <diogo.cardoso@replai.io>
2022-07-09 12:40:37 +0100

    update

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,7 +1,7 @@
 import { Cron, StackContext } from "@serverless-stack/resources";
 
-export function MyStack(this: any, { stack }: StackContext) {
-  new Cron(this, "Cron", {
+export function MyStack({ stack }: StackContext) {
+  new Cron(stack, "Cron", {
     schedule: "rate(1 hour)",
     job: "src/lambda.main",
   });
