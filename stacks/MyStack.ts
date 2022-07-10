040eb7ef251cb3427240f296a067615c3c71c602
Diogo Cardoso <diogocardoso92@outlook.com>
2022-07-10 18:38:45 +0100

    change schedule to 9am utc

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -2,7 +2,7 @@ import { Cron, StackContext } from "@serverless-stack/resources";
 
 export function MyStack({ stack }: StackContext) {
   new Cron(stack, "Cron", {
-    schedule: "cron(0 10 * * ? *)",
+    schedule: "cron(0 9 * * ? *)",
     job: "functions/lambda.main",
   });
 }
